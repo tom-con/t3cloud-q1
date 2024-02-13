e41b3a5ca1279e67a67330f0d56501122b81f8eb
tom-con <tconchie@gmail.com>
2024-02-13 11:09:18 +0600

    Adds a second loop (while loop)

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-var i = 0
+// var i = 0
 // i += 1
 // i = i + 1
 // i += 4
@@ -8,4 +8,11 @@ var i = 0
 // My very first loop
 for (var i = 0; i < 10; i += 1) {
 	console.log(i)
+}
+
+// My very second loop
+var i = 0
+while (i < 10) {
+	console.log(i)
+	i += 1
 }
