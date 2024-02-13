f07c2511a3b288514c6543af15c0211323a8127c
tom-con <tconchie@gmail.com>
2024-02-13 13:42:59 +0600

    Adds array file

---
+++ collection-data-types.js
@@ -0,0 +1,21 @@
+// Array is a list of other data types
+
+// var myEmptyArray = []
+
+var myNumberArray = [42, 1, 5, -8, 0.56]
+// console.log(myNumberArray[0])
+var myStrings = ["Dog", "Cat", "Egg", "Banana"]
+// console.log(myStrings[3])
+var myBools = [true, false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false]
+// console.log(myBools[myBools.length - 1])
+var myAnything = [4, false, "Cat", 0.423, ["Tom", 9]]
+// console.log(myAnything[4][1])
+var namesOfAttendees = ["Sangay", "Nayan", "Pema", "Sherab", "Passang", "Sonam", "Karma", "Chakar", "Tobdhen"]
+
+// for (var i = 0; i < namesOfAttendees.length; i += 1) {
+// 	var studentName = namesOfAttendees[i];
+// 	console.log(studentName + " is in class today.")
+// }
+
+
+
