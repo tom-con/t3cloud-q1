7c8dff071f6c5481c205a72a5d90d6ec578fd378
tom-con <tconchie@gmail.com>
2024-02-13 11:12:25 +0600

    Adds a third loop, (second while loop with user input)

--- loops.js
+++ loops.js
@@ -6,13 +6,24 @@
 
 
 // My very first loop
-for (var i = 0; i < 10; i += 1) {
-	console.log(i)
-}
+// for (var i = 0; i < 10; i += 1) {
+// 	console.log(i)
+// }
 
 // My very second loop
-var i = 0
-while (i < 10) {
-	console.log(i)
-	i += 1
-}
+// var i = 0
+// while (i < 10) {
+// 	console.log(i)
+// 	i += 1
+// }
+
+// var rl = require('readline-sync');
+
+// var shouldLoopRun = true
+
+// while (shouldLoopRun) {
+// 	var reply = rl.question("Should I ask another question?")
+// 	if (reply !== "yes") {
+// 		shouldLoopRun = false;
+// 	}
+// }
