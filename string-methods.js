a593a1628cb802942049e5575b891664410619c1
tom-con <tconchie@gmail.com>
2024-02-14 15:25:21 +0600

    Adds function for year to century with ordinals

--- string-methods.js
+++ string-methods.js
@@ -33,19 +33,49 @@
 // const capitalizedName = splitIntoWords.join(" ")
 // console.log(capitalizedName)
 
-function findLargestWord(sentence) {
-	var words = sentence.split(" ")
-	var largestWord = ""
+// function findLargestWord(sentence) {
+// 	var words = sentence.split(" ")
+// 	var largestWord = ""
 
-	for (var i = 0; i < words.length; i += 1) {
-		if (words[i].length > largestWord.length) {
-			largestWord = words[i]
-		}
+// 	for (var i = 0; i < words.length; i += 1) {
+// 		if (words[i].length > largestWord.length) {
+// 			largestWord = words[i]
+// 		}
+// 	}
+
+// 	return largestWord
+// }
+
+// console.log(findLargestWord("Hello how are you today"))
+// console.log(findLargestWord("It's really cold here today totals"))
+
+
+
+
+// 1616 - "17th century"
+// 1900 - "19th century"
+// 2007 - "21st century"
+
+// 1 - "1st century"
+// 100 - "2nd century"
+
+function getFromYear(year) {
+	const century = Math.ceil(year / 100)
+	var suffix = "th"
+	if (century % 100 >= 10 && century % 100 <= 20) {
+		suffix = "th"
+	} else if (century % 10 === 1) {
+		suffix = "st"
+	} else if (century % 10 === 2) {
+		suffix = "nd"
+	} else if (century % 10 === 3) {
+		suffix = "rd"
 	}
 
-	return largestWord
+	return century + suffix + " century"
 }
 
-console.log(findLargestWord("Hello how are you today"))
-console.log(findLargestWord("It's really cold here today totals"))
+console.log(getFromYear(1616)) // 17th
+
+
 
