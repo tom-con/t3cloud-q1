8fe79e9a25904a19b03610442539268e1df03731
tom-con <tconchie@gmail.com>
2024-02-14 09:26:45 +0600

    Adds try-catch file

---
+++ try-catch.js
@@ -0,0 +1,13 @@
+try {
+	// try {
+	i + 1
+	console.log("Will it print here?")
+	// } catch (e) {
+	// 	console.log("Here?")
+	// }
+} catch (e) {
+	console.log(e)
+}
+
+console.log("I want the program to continue")
+
