1040b4bde0fd6385defbfb0b6a81ebc91b8146e7
tom-con <tconchie@gmail.com>
2024-02-19 13:39:07 +0600

    Adds old school html+js

---
+++ website/code.js
@@ -0,0 +1,13 @@
+var count = 0;
+var isDisplayed = true;
+function onClick(e) {
+	// count += 1;
+	var smallHeader = document.getElementById("small-header")
+	// smallHeader.innerText = "I've been clicked " + count + " times."
+	smallHeader.style.display = isDisplayed ? "none" : "block"
+	isDisplayed = !isDisplayed
+}
+
+var myHeader = document.getElementById("my-header")
+
+myHeader.addEventListener('click', onClick)
