92d7163d0b9d603be854b90b8fe4d152b8947364
tom-con <tconchie@gmail.com>
2024-02-20 09:39:51 +0600

    Updates website dir to implement click behavior on all pages

--- website/code.js
+++ website/code.js
@@ -1,11 +1,11 @@
 var count = 0;
-var isDisplayed = true;
+// var isDisplayed = true;
 function onClick(e) {
-	// count += 1;
+	count += 1;
 	var smallHeader = document.getElementById("small-header")
-	// smallHeader.innerText = "I've been clicked " + count + " times."
-	smallHeader.style.display = isDisplayed ? "none" : "block"
-	isDisplayed = !isDisplayed
+	smallHeader.innerText = "I've been clicked " + count + " times."
+	// smallHeader.style.display = isDisplayed ? "none" : "block"
+	// isDisplayed = !isDisplayed
 }
 
 var myHeader = document.getElementById("my-header")
