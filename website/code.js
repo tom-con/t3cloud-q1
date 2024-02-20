fca7c3559a378a6e934ba2eb1c2a43967ea2cb36
tom-con <tconchie@gmail.com>
2024-02-20 10:26:22 +0600

    Adds basic sudoku implementation with html, css, js

--- website/code.js
+++ website/code.js
@@ -1,3 +1,34 @@
+const pages = [
+	{ name: 'Home', link: './index.html' },
+	{ name: 'Sudoku', link: './sudoku.html' },
+	{ name: 'Page 2', link: './page2.html' },
+	{ name: 'Page 3', link: './page3.html' },
+]
+
+const topNav = document.getElementById('top-nav')
+
+for (var i = 0; i < pages.length; i++) {
+	const newLink = document.createElement('a')
+	newLink.setAttribute('href', pages[i].link)
+	newLink.setAttribute('class', 'top-nav-link')
+	newLink.innerText = pages[i].name
+	topNav.appendChild(newLink)
+}
+
+const sudokuContainer = document.getElementById('sudoku-container')
+
+for (var i = 0; i < 9; i += 1) {
+	const newRow = document.createElement('div')
+	newRow.setAttribute('class', 'sudoku-row')
+	sudokuContainer.appendChild(newRow)
+	for (var j = 0; j < 9; j += 1) {
+		const newSquare = document.createElement('div')
+		newSquare.setAttribute('class', 'sudoku-square')
+		newRow.appendChild(newSquare)
+	}
+}
+
+
 var count = 0;
 // var isDisplayed = true;
 function onClick(e) {
